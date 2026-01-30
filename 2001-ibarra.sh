d764a73178dd741cca44aa1c41176acc63ce9957
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2026-01-30 13:45:54 -0600

    Fix filters

--- 2001-ibarra.sh
+++ 2001-ibarra.sh
@@ -46,7 +46,7 @@ do
         "delta"  : "$delta",
         "equinox": "2000"
       },
-      "command": "gridvisit 1 1 1 30 {{r g y} {r r y} {r i z}}",
+      "command": "gridvisit 1 1 1 30 {{g y} {r y} {i z}}",
       "estimatedduration": "3m"
     }
   ],
